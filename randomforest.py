2beb6f2fc243ef4a25da62116d95f0a226167e80
Satish Herakal <81845786+Satishherakal@users.noreply.github.com>
2026-03-09 16:16:58 +0530

    Add additional data points for training the classifier

--- randomforest.py
+++ randomforest.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-X = [[70],[80],[40],[60],[50],[39],[90]]
-y = [0,1,1,1,0,0,1]
+X = [[70],[80],[40],[60],[50],[39],[90],[70]]
+y = [0,1,1,1,0,0,1,1]
 classifier = RandomForestClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X,y)
 X_marks=[[36]]
